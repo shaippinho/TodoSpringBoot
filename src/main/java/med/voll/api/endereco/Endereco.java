2c271d762a737b6a369216a6997eb0ae2efbc579
Shaiene Pinho <t_shaiene.pinho@hapvida.com.br>
2023-05-26 12:31:24 -0300

    Migration

---
+++ src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,16 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class Endereco {
+
+    private String logradouro;
+    private String bairro;
+    private String cep;
+    private String numero;
+    private String complemento;
+    private String cidade;
+    private String uf;
+
+}
